9ab32b2b50f3026f226b341a77d3a0e70e553fce
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-06-09 11:36:49 -0500

    Entities and DAO animals

--- app/src/main/java/com/example/lab05_room/data/AnimalsDAO.kt
+++ app/src/main/java/com/example/lab05_room/data/AnimalsDAO.kt
@@ -1,4 +1,19 @@
 package com.example.lab05_room.data
 
-class AnimalsDAO {
+import androidx.room.*
+import com.example.lab05_room.data.entity.Animals
+
+@Dao
+interface AnimalsDAO {
+    @Query("SELECT * FROM Animals")
+    suspend fun getAll(): List<Animals>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(animal: Animals)
+
+    @Update
+    suspend fun update(animal: Animals)
+
+    @Delete
+    suspend fun delete(animal: Animals)
 }
