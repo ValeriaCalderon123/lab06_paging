ccd18701b9b43e50e63635d9f5be7de73ee7b726
ValeriaCalderon123 <valerianicoll.calderonr@gmail.com>
2022-06-09 11:35:34 -0500

    Second Changes

--- app/src/main/java/com/example/lab05_room/data/CategoryDAO.kt
+++ app/src/main/java/com/example/lab05_room/data/CategoryDAO.kt
@@ -1,4 +1,19 @@
 package com.example.lab05_room.data
 
-class CategoryDAO {
+import androidx.room.*
+import com.example.lab05_room.data.entity.Category
+
+@Dao
+interface CategoryDAO {
+    @Query("SELECT * FROM Category")
+    suspend fun getAll(): List<Category>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(animal: Category)
+
+    @Update
+    suspend fun update(animal: Category)
+
+    @Delete
+    suspend fun delete(animal: Category)
 }
--- app/src/main/java/com/example/lab05_room/data/entity/Category.kt
+++ app/src/main/java/com/example/lab05_room/data/entity/Category.kt
@@ -1,4 +1,12 @@
 package com.example.lab05_room.data.entity
 
-class Category {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Category (
+    @PrimaryKey(autoGenerate = true) val id_category: Int,
+    val name_category: String,
+    val quantity_animals_extinction: Int,
+    val state_category: Boolean
+)
