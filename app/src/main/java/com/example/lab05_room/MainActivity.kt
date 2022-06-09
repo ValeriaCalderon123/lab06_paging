b56750fbfe2eea7c09e8f271cd8f60afd8a28ecc
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-06-08 22:13:04 -0500

    Botones iniciales

--- app/src/main/java/com/example/lab05_room/MainActivity.kt
+++ app/src/main/java/com/example/lab05_room/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.lab05_room
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCRUDCategory: Button = findViewById(R.id.btnCRUDCategory)
+        val btnCRUDAnimals: Button = findViewById(R.id.btnCRUDAnimals)
+
+        btnCRUDCategory.setOnClickListener {
+            val intent = Intent(this, ListCategory::class.java)
+            startActivity(intent)
+        }
+
+        btnCRUDAnimals.setOnClickListener {
+            val intent = Intent(this, ListAnimals::class.java)
+            startActivity(intent)
+        }
     }
 }
