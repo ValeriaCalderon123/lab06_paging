3f79aa328bdd43ae90adaf9ad94177db1dd43b20
ValeriaCalderon123 <valerianicoll.calderonr@gmail.com>
2022-06-09 13:04:28 -0500

    Four Changes

--- app/src/main/java/com/example/lab05_room/data/SaveAnimalsDB.kt
+++ app/src/main/java/com/example/lab05_room/data/SaveAnimalsDB.kt
@@ -3,11 +3,9 @@ package com.example.lab05_room.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.lab05_room.data.entity.Category
+import com.example.lab05_room.data.entity.Animals
 
-@Database(
-    entities = [Category::class],
-    version = 2
-)
-abstract class SaveAnimalsDB : RoomDatabase() {
-    abstract fun categoryDao(): CategoryDAO
+@Database(entities = [Animals::class], version = 1)
+abstract class SaveAnimalsDB: RoomDatabase() {
+    abstract fun animalDao(): AnimalsDAO
 }
